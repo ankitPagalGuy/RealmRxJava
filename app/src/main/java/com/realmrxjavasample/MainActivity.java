510e516d88ec1ecac6b22f2a6a22b50b489a456e
ankitPagalGuy <ankit.choudhary@pagalguy.com>
2016-10-03 11:51:59 +0530

    main activity

--- app/src/main/java/com/realmrxjavasample/MainActivity.java
+++ app/src/main/java/com/realmrxjavasample/MainActivity.java
@@ -1,49 +1,51 @@
 package com.realmrxjavasample;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.Button;
+import com.realmrxjavasample.animation.AnimationActivity;
+import com.realmrxjavasample.gotchas.GotchasActivity;
+import com.realmrxjavasample.retrofit.RetrofitExample;
+import com.realmrxjavasample.throttle.ThrottleSearchActivity;
+import java.util.Map;
+import java.util.TreeMap;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
+  private ViewGroup container;
+  private final TreeMap<String, Class<? extends Activity>> buttons = new TreeMap<String, Class<? extends Activity>>() {{
+    put("Animation", AnimationActivity.class);
+    put("Throttle search", ThrottleSearchActivity.class);
+    put("Network", RetrofitExample.class);
+    put("Working with Realm", GotchasActivity.class);
+  }};
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-    setSupportActionBar(toolbar);
-
-    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-    fab.setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-            .setAction("Action", null)
-            .show();
-      }
-    });
+    container = (ViewGroup) findViewById(R.id.list);
+    setupButtons();
   }
 
-  @Override public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.menu_main, menu);
-    return true;
-  }
-
-  @Override public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
-      return true;
+  private void setupButtons() {
+    for (final Map.Entry<String, Class<? extends Activity>> entry : buttons.entrySet()) {
+      Button button = new Button(this);
+      button.setText(entry.getKey());
+      button.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+          startActivity(entry.getValue());
+        }
+      });
+      container.addView(button);
     }
+  }
 
-    return super.onOptionsItemSelected(item);
+  private void startActivity(Class<? extends Activity> activityClass) {
+    startActivity(new Intent(this, activityClass));
   }
 }
